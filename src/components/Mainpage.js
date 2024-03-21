82f861a7736af5360ab6cb4259bd4f97e565c478
ThanawaratGithub <lopamarkin@gmail.com>
2024-03-21 22:33:17 +0700

    add swipe

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -9,9 +9,10 @@ import line from "../assets/Line_logo.png";
 import BackToTopButton from "./Backtotop";
 import AnchorTemporaryDrawer from "./Drawer";
 import './Mainpage_mol.css'
+import SampleComponent from "./Swiper";
 import { useEffect } from "react";
 function MainPage() {
-  var isMobile = window.innerWidth <= 5000;
+  var isMobile = window.innerWidth <= 500;
 
   return (
     
@@ -23,7 +24,7 @@ function MainPage() {
       <svg xmlns="http://www.w3.org/2000/svg" width="29" height="19" viewBox="0 0 29 19" fill="none">
 <path d="M0.625 18.75H28.375V15.6667H0.625V18.75ZM0.625 11.0417H28.375V7.95833H0.625V11.0417ZM0.625 0.25V3.33333H28.375V0.25H0.625Z" fill="black"/>
 </svg>
-<AnchorTemporaryDrawer/>
+<AnchorTemporaryDrawer/>  
     </div>
     
     
@@ -34,6 +35,8 @@ function MainPage() {
     
     </>:<> <div className="lord">
         <div className="landlord">
+        <SampleComponent/>  
+
       <div className="header">
         <div className="logo_box">
           <img src={pic}></img>
---
+++ src/components/Swiper.js
@@ -0,0 +1,26 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './Swiper.css'
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+export default function SampleComponent() {
+  return (
+    <>
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper" style={{height:'500px'}}>
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
